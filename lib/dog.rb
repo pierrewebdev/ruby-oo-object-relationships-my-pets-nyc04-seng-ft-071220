f2f41f871bf9b6897b82fcc28f5da57ef03c1389
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-07 15:32:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,22 @@
 class Dog
   # code goes here
-  # code goes here
+  # class variable for all instances of dog
+  @@all = []
   
   #I don't think that the dog should be able to change names
   attr_reader :name
   #but their owner can change if they sell the dog
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   def initialize(name,owner)
     @name = name
     @owner =owner
+    @mood = "nervous"
   end
+  
+  def self.all
+    @@all
+  end
+  
+  
+  
 end
