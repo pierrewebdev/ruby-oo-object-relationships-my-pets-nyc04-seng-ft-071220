0a247d4acf939a3fc3613aa740ee819a5af16842
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-07 15:25:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,10 @@
 class Owner
   # code goes here
+  # code goes here
+  
+  #I don't think that the cat should be able to change names
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
