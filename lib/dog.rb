652eefa08254b16a8e251ea8c0fc2062ee1bf75c
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-07 15:24:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
   # code goes here
+  # code goes here
+  
+  #I don't think that the dog should be able to change names
+  attr_reader :name
+  #but their owner can change if they sell the dog
+  attr_accessor :owner
+  def initialize(name,owner)
+    @name = name
+    @owner =owner
+  end
 end
