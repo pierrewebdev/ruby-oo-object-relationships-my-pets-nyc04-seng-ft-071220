39cfe22594dc7faf4d90135a65d4003c10152140
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-07 15:26:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,8 +2,9 @@ class Owner
   # code goes here
   
   #I don't think that the owner should be able to change names
-  attr_reader :name
+  attr_reader :name :species
   def initialize(name)
     @name = name
+    @species = "human"
   end
 end
