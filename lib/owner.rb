50d6496d3e65cbb742af3f3582c4940d76604da3
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-07 15:27:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,15 @@
 class Owner
   # code goes here
   
+  #my class variable for all instance of owner
+  @@all = []
+  
   #I don't think that the owner should be able to change names
   attr_reader :name, :species
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end
   
   def say_species
