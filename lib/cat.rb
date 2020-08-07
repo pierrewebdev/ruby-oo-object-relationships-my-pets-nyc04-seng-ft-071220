2051a7c85086225e3da3a26d7964875db660178d
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-07 15:24:13 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
   # code goes here
+  
+  #I don't think that the cat should be able to change names
+  attr_reader :name
+  #but their owner can change if they sell the cat
+  attr_accessor :owner
+  def initialize(name,owner)
+    @name = name
+    @owner =owner
+  end
 end
